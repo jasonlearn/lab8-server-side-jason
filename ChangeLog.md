4fab7662579d01176342784b83e5b276c45300de
bohrium7 <bjdeith@gmail.com>
2017-04-08 10:59:00 -0700

    Added backend functionality

---
+++ ChangeLog.md
@@ -0,0 +1,14 @@
+# Project Information
+## Contributors
+- Braden D'Eith  (bohrium7) 
+    - Captain, Backend
+- Jason Chan     (jasonlearn)
+     - Mate, Frontend
+
+# Changelog
+## [1.0] - 2017-04-08
+### New Features
+- Added back end functionality
+- Added RESTful controller
+- simplified UI of backend
+- Added REST library
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,19 +6,11 @@ class Welcome extends Application {
 
 	public function index()
 	{
-		$result = '';
-		$oddrow = true;
-		foreach ($this->categories->all() as $category)
-		{
-//			$viewparms = array(
-//				'direction' => ($oddrow ? 'left' : 'right')
-//			);
-//			$viewparms = array_merge($viewparms, $category);
-			$category->direction = ($oddrow ? 'left' : 'right');
-			$result .= $this->parser->parse('category-home', $category, true);
-			$oddrow = ! $oddrow;
-		}
-		$this->data['content'] = $result;
+                $this->load->helper('formfields');
+                $this->data['title'] = 'Diner (Server)';
+                $this->data['pagebody'] = 'welcome_message';
+            
+            
 		$this->render();
 	}
 
